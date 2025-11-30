4113f1f1d5f8762fdebbec74884666432bd15b6c
jj975 <bpoluhovich2312@gmail.com>
2025-11-30 02:28:06 +0200

    update admin.py

--- lab6_banking/mysite/app_blog/admin.py
+++ lab6_banking/mysite/app_blog/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Article, ArticleImage, Category
 
-# Register your models here.
+# Дозволяє додавати картинки прямо на сторінці статті
+
+
+class ArticleImageInline(admin.TabularInline):
+    model = ArticleImage
+    extra = 0
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'slug', 'main_page')
+    inlines = [ArticleImageInline]
+    multiupload_form = True
+    multiupload_list = False
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('category',)
+    fieldsets = (
+        ('', {
+            'fields': ('pub_date', 'title', 'description', 'main_page'),
+        }),
+        (('Додатково'), {
+            'classes': ('grp-collapse grp-closed',),
+            'fields': ('slug',),
+        }),
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category', 'slug')
+    prepopulated_fields = {'slug': ('category',)}
+    fieldsets = (
+        ('', {
+            'fields': ('category', 'slug'),
+        }),
+    )
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
