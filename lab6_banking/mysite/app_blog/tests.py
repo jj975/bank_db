45a3e11b510dc0c95366426a3286a7b186fb8b54
jj975 <bpoluhovich2312@gmail.com>
2025-11-30 04:05:32 +0200

    update tests for new python santacsis

--- lab6_banking/mysite/app_blog/tests.py
+++ lab6_banking/mysite/app_blog/tests.py
@@ -6,4 +6,4 @@ class HomeTests(TestCase):
     def test_home_view_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
